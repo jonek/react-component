1987e4bcf2a50ee7dab8ef3991c1e2a6caf47bd6
Johannes Eickhold <jeick@eclipsesource.com>
2021-01-04 16:58:32 +0100

    Updates

---
+++ precache-manifest.e2fcd8ae852bcd36c9748e5df2b5110e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "997856f12117ef5296457f7a1deb3ac2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7467c4f917471273d8a6",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "bf6d0adae2bc26967384",
+    "url": "./static/js/2.53427459.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.53427459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7467c4f917471273d8a6",
+    "url": "./static/js/main.d57ea4f7.chunk.js"
+  },
+  {
+    "revision": "d552d8c2ec77ac2fa593",
+    "url": "./static/js/runtime-main.6c7d6f2b.js"
+  }
+]);
