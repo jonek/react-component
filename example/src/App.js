072ee510d51f7d1f7ce466ee43bf5ee99ecca315
Johannes Eickhold <jeick@eclipsesource.com>
2021-01-05 15:15:10 +0100

    Enable styling of component by consumer

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react'
-
-import { ExampleComponent } from 'component'
 import 'component/dist/index.css'
 
+import { ExampleComponent } from 'component'
+import React from 'react'
+
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent className="yellow" text="Create React Library Example 😄" />
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>My React Example Component: {text}</div>
+export const ExampleComponent = ({ text, className }) => {
+  return (
+    <div className={`${styles.test} ${className}`}>
+      My React Example Component: {text}
+    </div>
+  )
 }
